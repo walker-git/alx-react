1cb3d0fc05b056607ccb32fafabd8213949dde13
walker-git <setiks92@gmail.com>
2023-08-24 15:44:02 -0700

    map

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,5 @@
 import { Map } from 'immutable';
 
-const getImmutableObject = (object) => Map(object);
-
-module.exports = getImmutableObject;
+export default function getImmutableObject (object) {
+  return Map(object);
+}
